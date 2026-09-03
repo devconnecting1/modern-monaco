133b1fb16cbd12e842bb41f7a2309d5ecb6b76c9
mmdj04 <mmdj04@users.noreply.github.com>
2026-09-03 13:56:48 +0000

    feat: add Vercel deployment config and demo landing page

---
+++ public/js/ssr.mjs
@@ -0,0 +1,23 @@
+import { renderToWebComponent } from "../../dist/ssr/index.mjs";
+import { files } from "./files.mjs";
+
+const filename = "src/App.tsx";
+
+export default {
+  async fetch(req) {
+    const ssrOutput =  await renderToWebComponent(
+      { filename, code: files[filename] },
+      {
+        padding: { top: 8, bottom: 8 },
+        userAgent: req.headers.get("user-agent"),
+      }
+    );
+    const html = await Bun.file(new URL("../ssr.html", import.meta.url)).text();
+    return new Response(html.replace("{SSR}", ssrOutput), {
+      headers: {
+        "cache-control": "public, max-age=0, revalidate",
+        "content-type": "text/html; charset=utf-8",
+      },
+    });
+  }
+}
